431ba3380b12967a9b71a74a65cd6e1da6117e5e
ToshihikoSakai <s73041h20@yahoo.co.jp>
2022-01-03 16:58:35 +0900

    transform Japanase to English

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# jsontoconll
-doccano jsonl to conll file
+# JsonToConll
+JsonToConll can transform [doccano](https://github.com/doccano/doccano) jsonl file to conll file.
+
+# Usage
+```
+python3 jsontoconll.py ./data/admin_new.jsonl > ./data/dataset.conll
+```
+
--- jsontoconll.py
+++ jsontoconll.py
@@ -12,53 +12,24 @@ import sys
 
 tagger = MeCab.Tagger("-Owakati -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
 
-# doccanoのjsonファイルを読み込む
+# Read jsonl file from doccano
 pdjson = pd.read_json(sys.argv[1],orient='records', lines=True)
 
 
-# "labels"の2次元配列の要素内を昇順に並び替える
+# Sort the elements "labels" in ascending order
 pdjson['labels'] = [sorted(l) for l in pdjson['labels']]
 
-labels = []
-words = []
-
-newlabels = []
-
-def Morphological_jsonl_to_jsonl(pdjson):
-
-    splitted = []
-    position = 0
-    
-    # jsonl file each row
-    for index,row in pdjson.iterrows():
-        text = row['text']
-        
-        for label in row['labels']:
-            start = label[0]
-            end = label[1]
-            tag = label[2]
-            splitted.append({'text':text[position:start], 'label':0})
-            splitted.append({'text':text[start:end], 'label':tag})
-            position = end
-        splitted.append({'text': text[position:], 'label':0})
-        print(splitted)
-
-        
 def PrintConll(text,words,labels):
-
 
     for w in text.split():
         flag = 0
         for tagw,tag in zip(words,labels):
 
-            # 形態素解析
+            # morphological analysis
             parsew = tagger.parse(w)
             
-            # 単語がタグの単語と一致したら
+            # match words and tag's words
             if(w == tagw):
-                #print("{} {}".format(w,tag))
-
-                #print("parsew={}".format(parsew))
                 headflag = 0
                 for pw in parsew.split():
                     if(headflag==0):
@@ -69,13 +40,13 @@ def PrintConll(text,words,labels):
                 flag = 1
                 break
             
-        # 単語がタグでなかったら
+        # word is not tag
         if(flag==0):
             for pw in parsew.split():
                 print("{} O".format(pw))
 
 
-#Morphological_jsonl_to_jsonl(pdjson)
+
 
 def WordsLabels(text,labels):
 
@@ -87,7 +58,7 @@ def WordsLabels(text,labels):
         end = label[1]
         tag = label[2]
         
-        # labelsと単語を格納
+        # Store labels and words
         labels_list.append(tag)
         words.append(text[start:end])
         
@@ -96,26 +67,18 @@ def WordsLabels(text,labels):
 for index,row in pdjson.iterrows():
     position = 0
 
-    # タグの単語とラベルを格納
+    # Store tag's words and labels
     words,labels = WordsLabels(row['text'],row['labels'])
 
-    # タグがついた単語を残し、単語の前後に空白を挿入
+    # Remain tag's words and insert space before and after the words
     for w,tag in zip(words,labels):
-        #print("{} {}".format(w,tag))
         p = re.compile(re.escape(w))
 
-        #print("type={}".format(type(row['text'])))
-        #print(row['text'])
-
         for m in p.finditer(row['text']):
-            #print("m={}".format(m))
-            #print("m.start()={}".format(m.start()))
-            #print("m.end()={}".format(m.end()))
             str_list = list(row['text'])
             
-            # position以降を検索
+            # Search after position
             if(m.start() >= position):
-                #print("postion={}".format(position))
                 str_list.insert(int(m.end()),' ')
                 str_list.insert(int(m.start()),' ')
                 str_list = ''.join(str_list)
@@ -126,10 +89,10 @@ for index,row in pdjson.iterrows():
 
         row['text'] = str_list
 
-    # 複数続いた空白を一つへする
+    # Combine multiple consecutive blanks into one
     row['text'] = " ".join(row['text'].split())
 
-    # Conll形式の出力
+    # Print Conll
     PrintConll(row['text'],words,labels)
     print("")
 
