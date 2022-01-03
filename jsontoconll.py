b6ebc7ba032a226962bfa54217f6e9972b4fe737
ToshihikoSakai <s73041h20@yahoo.co.jp>
2022-01-03 16:39:22 +0900

    jsonlからconll形式へ正しく変換する

--- jsontoconll.py
+++ jsontoconll.py
@@ -19,65 +19,117 @@ pdjson = pd.read_json(sys.argv[1],orient='records', lines=True)
 # "labels"の2次元配列の要素内を昇順に並び替える
 pdjson['labels'] = [sorted(l) for l in pdjson['labels']]
 
-
 labels = []
 words = []
 
 newlabels = []
 
-for index,row in pdjson.iterrows():
-    # Morphological analysis
-    parseline = tagger.parse(row['text']).rstrip()
+def Morphological_jsonl_to_jsonl(pdjson):
 
-    # labelsの単語を認識する
-    for label in row['labels']:
+    splitted = []
+    position = 0
+    
+    # jsonl file each row
+    for index,row in pdjson.iterrows():
+        text = row['text']
+        
+        for label in row['labels']:
+            start = label[0]
+            end = label[1]
+            tag = label[2]
+            splitted.append({'text':text[position:start], 'label':0})
+            splitted.append({'text':text[start:end], 'label':tag})
+            position = end
+        splitted.append({'text': text[position:], 'label':0})
+        print(splitted)
+
+        
+def PrintConll(text,words,labels):
+
+
+    for w in text.split():
+        flag = 0
+        for tagw,tag in zip(words,labels):
+
+            # 形態素解析
+            parsew = tagger.parse(w)
+            
+            # 単語がタグの単語と一致したら
+            if(w == tagw):
+                #print("{} {}".format(w,tag))
+
+                #print("parsew={}".format(parsew))
+                headflag = 0
+                for pw in parsew.split():
+                    if(headflag==0):
+                        print("{} B-{}".format(pw,tag))
+                        headflag = 1
+                    else:
+                        print("{} I-{}".format(pw,tag))                
+                flag = 1
+                break
+            
+        # 単語がタグでなかったら
+        if(flag==0):
+            for pw in parsew.split():
+                print("{} O".format(pw))
+
+
+#Morphological_jsonl_to_jsonl(pdjson)
+
+def WordsLabels(text,labels):
+
+    words = []
+    labels_list = []
+    
+    for label in labels:
         start = label[0]
         end = label[1]
         tag = label[2]
         
         # labelsと単語を格納
-        labels.append(tag)
-        words.append(row['text'][start:end]) 
-    
-    # 形態素解析したテキストへ置換
-    pdjson.at[index,'text'] = parseline
+        labels_list.append(tag)
+        words.append(text[start:end])
+        
+    return words,labels_list
 
-    # labelsの単語とマッチした位置を新たにlabelsの位置とする
+for index,row in pdjson.iterrows():
     position = 0
+
+    # タグの単語とラベルを格納
+    words,labels = WordsLabels(row['text'],row['labels'])
+
+    # タグがついた単語を残し、単語の前後に空白を挿入
     for w,tag in zip(words,labels):
-        
-        ws = tagger.parse(w).rstrip()
+        #print("{} {}".format(w,tag))
+        p = re.compile(re.escape(w))
 
-        # wsにマッチする単語を検索
-        p = re.compile(re.escape(ws))
+        #print("type={}".format(type(row['text'])))
+        #print(row['text'])
 
-        # ヒットした中を検索
-        for m in p.finditer(parseline):
-            # position以降を検索
-            if((position != 0) and (m.start() < position)):
-                continue
-            start = m.start()
-            end = m.end()
-            break
+        for m in p.finditer(row['text']):
+            #print("m={}".format(m))
+            #print("m.start()={}".format(m.start()))
+            #print("m.end()={}".format(m.end()))
+            str_list = list(row['text'])
             
-        # 新しいlabelsを設定
-        tmp_label = []
-        tmp_label.append(start)
-        tmp_label.append(end)
-        tmp_label.append(str(tag))
-        newlabels.append(tmp_label)
-        position = end
+            # position以降を検索
+            if(m.start() >= position):
+                #print("postion={}".format(position))
+                str_list.insert(int(m.end()),' ')
+                str_list.insert(int(m.start()),' ')
+                str_list = ''.join(str_list)
+                position = int(m.end())
+                break
+            else:
+                str_list = ''.join(str_list)
 
+        row['text'] = str_list
 
-    # 形態素解析したテキストへ置換
-    pdjson.at[index,'labels'] = sorted(newlabels)
+    # 複数続いた空白を一つへする
+    row['text'] = " ".join(row['text'].split())
 
+    # Conll形式の出力
+    PrintConll(row['text'],words,labels)
+    print("")
 
-    # 辞書と配列の初期化
-    labels = []
-    words = []
-    newlabels = []
-    position = 0
-        
-
-pdjson.to_json(sys.argv[2],force_ascii=False,orient='records',lines=True)
